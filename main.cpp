c64599e13d38fbf4b5abaa971d52348dafe185ed
Danny Feng <fym8888@gmail.com>
2019-10-18 16:49:43 +1100

    done

--- main.cpp
+++ main.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
 #include<fstream>
-//#include<string>
 #include <vector>
-//#include <list>
-//#include <unordered_map>
 
 using namespace std;
-//struct Model {
-//    int group;
-//    int index;
-//    int price;
-//};
-//unordered_map<int, list<Model>> table;
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         cout << "Error: Input file not specified or too many arguments" << endl << "Usage: Program FileName" << endl;
@@ -24,7 +16,6 @@ int main(int argc, char **argv) {
         return -1;
     }
     int budget, buyTypeQty, modelCount;
-//    string i;
     fin >> budget >> buyTypeQty;
     vector<int> table[buyTypeQty]; // array of vectors
     int group = 0;
@@ -38,33 +29,38 @@ int main(int argc, char **argv) {
         table[group] = models;
         group++;
     }
+    int n = group;
+    int *indexMask = new int[n];
+    for (int i = 0; i < n; i++)
+        indexMask[i] = 0;
+    int bestCost = 0;
+    int selectedIndex[n];
+    int tmpSelectedIndex[n];
+    while (true) {
+        int tempCost = 0;
+        for (int i = 0; i < n; i++) {
+            tempCost += table[i][indexMask[i]];
+            tmpSelectedIndex[i] = indexMask[i];
+        }
+        if (tempCost <= budget && tempCost > bestCost) {
+            bestCost = tempCost;
+            for (int i = 0; i < n; i++)
+                selectedIndex[i] = tmpSelectedIndex[i];
+        }
 
-//    int n = group;
-//    int* indices = new int[n];
-//    for (int i = 0; i < n; i++)
-//        indices = 0;
-//    while (1) {
-//        for (int i = 0; i < n; i++) {
-//            list<Model>::iterator it = table[i].begin();
-//            advance (it, indices[i]);
-//            cout << it->price << " ";
-//            cout << endl;
-//        }
-//
-//
-//        int next = n - 1;
-//        while (next >= 0 && (indices[next] + 1 >= table[next].size()))
-//            next --;
-//        if (next < 0)
-//            break;
-//        indices[next]++;
-//
-//        for (int i = next +1; i < n; i++)
-//            indices[i] = 0;
-//    }
+        int next = n - 1;
+        while (next >= 0 && (indexMask[next] + 1 >= table[next].size()))
+            next--;
+        if (next < 0)
+            break;
+        indexMask[next]++;
 
-//    list<Model>::iterator it;
-//    for(it = table[25].begin(); it != table[25].end(); ++it)
-//        cout << it -> price << " ";
+        for (int i = next + 1; i < n; i++)
+            indexMask[i] = 0;
+    }
+    cout << bestCost << " ";
+    for (int i = 0; i < n; i++)
+        cout << selectedIndex[i] + 1 << " ";
+    cout << endl;
     return 0;
 }
