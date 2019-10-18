3e81f3d74136629420451288ed3ff8b01b11d44a
Danny Feng <fym8888@gmail.com>
2019-10-18 16:10:39 +1100

    change to use an array of vectors to hold data

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include<fstream>
-#include<string>
-#include <list>
-#include <unordered_map>
+//#include<string>
+#include <vector>
+//#include <list>
+//#include <unordered_map>
 
 using namespace std;
-struct Model {
-    int group;
-    int index;
-    int price;
-};
-unordered_map<int, list<Model>> table;
-
+//struct Model {
+//    int group;
+//    int index;
+//    int price;
+//};
+//unordered_map<int, list<Model>> table;
 int main(int argc, char **argv) {
     if (argc != 2) {
         cout << "Error: Input file not specified or too many arguments" << endl << "Usage: Program FileName" << endl;
@@ -24,23 +24,47 @@ int main(int argc, char **argv) {
         return -1;
     }
     int budget, buyTypeQty, modelCount;
-    string i;
+//    string i;
     fin >> budget >> buyTypeQty;
-
+    vector<int> table[buyTypeQty]; // array of vectors
     int group = 0;
     while (fin >> modelCount) {
-        group++;
-        list<Model> models;
-        for (int modelIndex = 1; modelIndex <= modelCount; modelIndex++) {
+        vector<int> models(modelCount); // array element of table
+        for (int modelIndex = 0; modelIndex < modelCount; modelIndex++) {
             int price;
             fin >> price;
-            Model currentModel = {group, modelIndex, price};
-            models.push_back(currentModel);
+            models[modelIndex] = price;
         }
         table[group] = models;
+        group++;
     }
-    list<Model>::iterator it;
-    for(it = table[25].begin(); it != table[25].end(); ++it)
-        cout << it -> price << " ";
+
+//    int n = group;
+//    int* indices = new int[n];
+//    for (int i = 0; i < n; i++)
+//        indices = 0;
+//    while (1) {
+//        for (int i = 0; i < n; i++) {
+//            list<Model>::iterator it = table[i].begin();
+//            advance (it, indices[i]);
+//            cout << it->price << " ";
+//            cout << endl;
+//        }
+//
+//
+//        int next = n - 1;
+//        while (next >= 0 && (indices[next] + 1 >= table[next].size()))
+//            next --;
+//        if (next < 0)
+//            break;
+//        indices[next]++;
+//
+//        for (int i = next +1; i < n; i++)
+//            indices[i] = 0;
+//    }
+
+//    list<Model>::iterator it;
+//    for(it = table[25].begin(); it != table[25].end(); ++it)
+//        cout << it -> price << " ";
     return 0;
 }
